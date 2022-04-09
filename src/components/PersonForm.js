6b191d7b195de66c23599f36389cddfb36bab9e6
thomas-mich <97365235+tomcodes90@users.noreply.github.com>
2022-04-10 00:09:10 +0200

    update

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -1,32 +1,72 @@
 import Card from "./Card";
 import "./PersonForm.css";
+import { useState } from "react";
 
-function PersonForm(props) {
+function PersonForm() {
+  const [personName, setPersonName] = useState("");
+  const inputPersonName = (event) => {
+    setPersonName(event.target.value);
+  };
+  const [personJob, setPersonJob] = useState("");
+  const inputPersonJob = (event) => {
+    setPersonJob(event.target.value);
+  };
+  const [personAge, setPersonAge] = useState("");
+  const inputPersonAge = (event) => {
+    setPersonAge(event.target.value);
+  };
+  const [personNetworth, setPersonNetworth] = useState("");
+  const inputPersonNetworth = (event) => {
+    setPersonNetworth(event.target.value);
+  };
+  const [personCountry, setPersonCountry] = useState("");
+  const inputPersonCountry = (event) => {
+    setPersonCountry(event.target.value);
+  };
+  const submitValue = (event) => {
+    event.preventDefault();
+    const personData = {
+      name: personName,
+      job: personJob,
+      age: personAge,
+      networth: personNetworth,
+      country: personCountry,
+    };
+    console.log(personData);
+  };
   return (
     <>
-      <Card className="person-form">
-        <input placeholder="Name" />
-        <input placeholder="Job" />
-        <input placeholder="Age" />
-        <input placeholder="Networth" />
-        <input placeholder="Country" />
-        <button onClick={() => props.addUserProp()}>Add user</button>
-        <p id="person__name" className="no-display">
+      <form onSubmit={submitValue} className="person-form">
+        <input onChange={inputPersonName} id="person-name" placeholder="Name" />
+        <input onChange={inputPersonJob} id="person-job" placeholder="Job" />
+        <input onChange={inputPersonAge} id="person-age" placeholder="Age" />
+        <input
+          onChange={inputPersonNetworth}
+          id="person-networth"
+          placeholder="Networth"
+        />
+        <input
+          onChange={inputPersonCountry}
+          id="person-country"
+          placeholder="Country"
+        />
+        <button>Add user</button>
+        <p id="person-name__error" className="no-display">
           Please insert name
         </p>
-        <p id="person__job" className="no-display">
+        <p id="person-job__error" className="no-display">
           Please insert job
         </p>
-        <p id="person__age" className="no-display">
+        <p id="person-age__error" className="no-display">
           Please insert age
         </p>
-        <p id="person__networth" className="no-display">
+        <p id="person-networth__error" className="no-display">
           Please insert networth
         </p>
-        <p id="person__country" className="no-display">
+        <p id="person-country__error" className="no-display">
           Please insert country
         </p>
-      </Card>
+      </form>
     </>
   );
 }
